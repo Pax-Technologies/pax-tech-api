4d1d10aaecbcb7aaa3c39818c09534eb5c733c73
PaxBryan <84713146+PaxBryan@users.noreply.github.com>
2024-09-23 13:45:16 +0200

    Add htacces + BlogCrudController.php adaptation for CKeditorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/BlogCrudController.php
+++ src/Controller/Admin/BlogCrudController.php
@@ -3,18 +3,34 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Blog;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class BlogCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Blog::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+//            ->setEntityLabelInSingular('Demande de contact')
+//            ->setEntityLabelInPlural('Demandes de contact')
+//
+//            ->setPageTitle("index", "SymRecipe - Administration des demandes de contact")
+//
+//            ->setPaginatorPageSize(20)
+
+            ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
     }
 
 
@@ -24,7 +40,8 @@ class BlogCrudController extends AbstractCrudController
             TextField::new('title'),
             TextField::new('subtitle'),
             DateField::new('creation_date'),
-            TextEditorField::new('content'),
+            TextareaField::new('content')->setFormType(CKEditorType::class)
+                ->hideOnIndex(),
             ImageField::new('image_name')
                 ->setBasePath('uploaded/images')
                 ->setUploadDir('public/uploaded/images')
