ed97df8e1bc96178e865c0a81ea6ccb2c219e5f5
PaxBryan <84713146+PaxBryan@users.noreply.github.com>
2024-09-27 13:45:39 +0200

    Add email sending for email configuration

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Blog;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
@@ -33,16 +37,47 @@ class DashboardController extends AbstractDashboardController
         return $this->render('admin/dashboard.html.twig');
     }
 
+    // Méthode pour l'envoi d'email
+    #[Route('/send-email', name: 'send_email_route', methods: ['POST'])]
+    public function sendEmail(Request $request, MailerInterface $mailer): Response
+    {
+        $emailAddress = $request->request->get('email');
+
+        // Créer le contenu HTML
+        $htmlContent = $this->renderView('emails/template.html.twig', [
+            'name' => 'Utilisateur',
+        ]);
+
+        // Créer et envoyer l'email
+        $email = (new Email())
+            ->from('support@pax-tech.com')
+            ->to($emailAddress)
+            ->subject('Configuration des emails')
+            ->html($htmlContent);
+
+        $mailer->send($email);
+        $this->addFlash('success', 'Email envoyé avec succès à ' . $emailAddress);
+
+        return $this->redirectToRoute('admin'); // Rediriger vers le dashboard
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Pax Tech Api');
+            ->setTitle('<img src="Logos/Web/PNG/RGB Negatif/300/PaxTech-Horizontal-Negatif-RGB-300[Cropped].png" height="75">')
+            ->setFaviconPath('favicon.ico')
+        ;
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('Console', 'fa fa-home');
         yield MenuItem::linkToCrud('Blog', 'fa fa-book', Blog::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('css/admin.css');
     }
 }
