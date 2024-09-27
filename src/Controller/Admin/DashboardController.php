4bc000f31d114ecf85dfc785962f748525bd0d10
PaxBryan <84713146+PaxBryan@users.noreply.github.com>
2024-09-27 15:09:53 +0200

    Add login form (security, user)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -78,6 +78,6 @@ class DashboardController extends AbstractDashboardController
 
     public function configureAssets(): Assets
     {
-        return Assets::new()->addCssFile('css/admin.css');
+        return Assets::new()->addCssFile('styles/admin.styles');
     }
 }
